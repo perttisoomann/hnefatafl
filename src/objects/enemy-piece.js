ded23e50cc1285730565c80d7d6d7f26d0447099
Pertti Soomann <psoomann@jfd.co.uk>
2025-03-25 14:13:29 +0000

    Stop enemy pieces going to corner pieces

--- src/objects/enemy-piece.js
+++ src/objects/enemy-piece.js
@@ -30,4 +30,46 @@ class EnemyPiece extends Piece {
                 break;
         }
     }
+
+    getValidMoves() {
+        let moves = [];
+
+        const boardRows = this.board.rows;
+        const boardCols = this.board.cols;
+        const cornerPositions = [
+            [0, 0], [0, boardCols - 1],
+            [boardRows - 1, 0], [boardRows - 1, boardCols - 1]
+        ];
+
+        const isCorner = (r, c) => cornerPositions.some(([cr, cc]) => cr === r && cc === c);
+
+        // Check vertical movement
+        for (let i = this.row - 1; i >= 0; i--) {
+            if (this.board.tiles[i][this.col].piece) break;
+            if (!isCorner(i, this.col)) {
+                moves.push([i, this.col]);
+            }
+        }
+        for (let i = this.row + 1; i < this.board.rows; i++) {
+            if (this.board.tiles[i][this.col].piece) break;
+            if (!isCorner(i, this.col)) {
+                moves.push([i, this.col]);
+            }
+        }
+
+        // Check horizontal movement
+        for (let j = this.col - 1; j >= 0; j--) {
+            if (this.board.tiles[this.row][j].piece) break;
+            if (!isCorner(this.row, j)) {
+                moves.push([this.row, j]);
+            }
+        }
+        for (let j = this.col + 1; j < this.board.cols; j++) {
+            if (this.board.tiles[this.row][j].piece) break;
+            if (!isCorner(this.row, j)) {
+                moves.push([this.row, j]);
+            }
+        }
+        return moves;
+    }
 }
