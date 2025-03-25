3bdaf551698fb9f802ce33025d1723adec21922b
Pertti Soomann <psoomann@jfd.co.uk>
2025-03-25 13:46:08 +0000

    Reset board properly after a win

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1175,10 +1175,22 @@ class VikingChess extends Phaser.Scene {
         // Reset board (but don't destroy it)
         this.board.resetBoard();
 
+        const centerCol = Math.floor(this.board.cols / 2);
+        const centerRow = Math.floor(this.board.rows / 2);
+
         // Move remaining player pieces to the center
         const centerPositions = [
-            [2, 3], [3, 2], [4, 3], [3, 4], // Cardinal directions
-            [2, 2], [2, 4], [4, 2], [4, 4]  // Diagonals
+            // Cardinal directions
+            [centerRow - 1, centerCol],
+            [centerRow, centerCol - 1],
+            [centerRow, centerCol + 1],
+            [centerRow + 1, centerCol],
+
+            // Diagonals
+            [centerRow - 1, centerCol - 1],
+            [centerRow - 1, centerCol + 1],
+            [centerRow + 1, centerCol - 1],
+            [centerRow + 1, centerCol + 1],
         ];
 
         this.playerPieces.forEach((piece, index) => {
@@ -1194,19 +1206,34 @@ class VikingChess extends Phaser.Scene {
 
         // Recreate king in center if it was captured
         if (!this.kingPiece.sprite || !this.kingPiece.sprite.active) {
-            this.kingPiece = new KingPiece(this, this.board, 3, 3);
+            this.kingPiece = new KingPiece(this, this.board, centerRow, centerCol);
         } else {
-            this.kingPiece.row = 3;
-            this.kingPiece.col = 3;
-            const { x, y } = this.board.getTilePosition(3, 3);
+            this.kingPiece.row = centerRow;
+            this.kingPiece.col = centerCol;
+            const { x, y } = this.board.getTilePosition(centerRow, centerCol);
             this.kingPiece.sprite.setPosition(x, y);
-            this.board.tiles[3][3].piece = this.kingPiece;
+            this.board.tiles[centerRow][centerCol].piece = this.kingPiece;
         }
 
         // Recreate enemy pieces in original positions
         this.enemyPieces = [];
         const enemyPositions = [
-            [0, 2], [0, 3], [0, 4], [3, 0], [3, 6], [6, 2], [6, 3], [6, 4]
+            // Top
+            [0, centerCol - 1],
+            [0, centerCol],
+            [0, centerCol + 1],
+
+            // Left
+            [centerRow, 0],
+
+            // Right
+            [centerRow, this.board.cols - 1],
+
+            // Bottom
+            [this.board.rows - 1, centerCol - 1],
+            [this.board.rows - 1, centerCol],
+            [this.board.rows - 1, centerCol + 1],
+            [this.board.rows - 1, centerCol + 1],
         ];
         enemyPositions.forEach(([row, col]) => {
             this.enemyPieces.push(new EnemyPiece(this, this.board, row, col));
